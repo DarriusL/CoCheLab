57356ffbf1ad9695331abcbd7cffaf766e6116de
DarriusL <darrius.lei@outlook.com>
2023-03-16 21:06:21 +0800

    Add files via upload

---
+++ lib/callback.py
@@ -0,0 +1,80 @@
+# @Time   : 2022/4/12
+# @Author : Junwei Lei
+# @Email  : darrius.lei@outlook.com
+
+import logging, os
+
+class CustomException(Exception):
+    ''' Used to output exception information if an exception is thrown
+
+    Parameters:
+    -----------
+
+    Exception: str
+        The exception information output when an exception is actively thrown
+    
+    Examples:
+    ---------
+    >>> import lib.callback.CustomException as ce
+    >>> raise ce('error')
+
+    '''
+    def __init__(self,ErrorInfo):
+        self.ErrorInfo = '\033[0;31m' + str(ErrorInfo) + '\033[0m';
+    def __str__(self):
+        return self.ErrorInfo
+
+
+class Logger():
+    '''Logger class: logging that can be output on the console and logger files at the same time
+
+    Parameters:
+    -----------
+
+    level:
+    Default level of logging output
+    default: logging.INFO
+
+    filename:
+    path of the logger file
+    default:'./cache/logger/logger.log'
+
+    Example:
+    --------
+    >>> import lib.callback.Logger
+    >>> log = Logger().get_log()
+    >>> log.info('Example complete.')
+    '''
+    def __init__(self, level = logging.INFO, filename = './cache/logger/logger.log') -> None:
+        #format for the logger
+        self.format = logging.Formatter(fmt = '\n[%(asctime)s]-[%(levelname)s - %(name)s] \n%(message)s');
+        self.level = level;
+        path, _ = os.path.split(filename);
+        if not os.path.exists(path):
+            raise CustomException(f'Path({path}) does not exist');
+        self.filename = filename;
+        #Create a logger object
+        self.logger = logging.getLogger(__name__);
+        self.logger.setLevel(self.level);
+
+    def get_console_handler(self):
+        #Create a log handler for the console
+        console_handler = logging.StreamHandler();
+        console_handler.setLevel(self.level);
+        console_handler.setFormatter(self.format);
+        return console_handler;
+
+    def get_file_handler(self):
+        #Create a log handler for the file
+        file_handler = logging.FileHandler(
+            filename = self.filename,
+            mode = 'w'
+        )
+        file_handler.setLevel(self.level);
+        file_handler.setFormatter(self.format);
+        return file_handler;
+
+    def get_log(self):
+        self.logger.addHandler(self.get_console_handler());
+        self.logger.addHandler(self.get_file_handler());
+        return self.logger;
---
+++ lib/glb_var.py
@@ -0,0 +1,45 @@
+# @Time   : 2022/4/12
+# @Author : Junwei Lei
+# @Email  : darrius.lei@outlook.com
+
+from lib.callback import CustomException
+'''
+Store global variables for the project
+
+Methods:
+--------
+
+__init__():None
+    initialization
+
+set_value(key, value):None
+    set global variables
+
+get_value(key):value
+    retrieve global variables
+
+Examples:
+---------
+@main.py
+>>> import lib.glb_var
+>>> glb_var.set_value('a', 1);
+>>> submain;
+
+@submain.py
+>>> import lib.glb_var
+>>> print(glb_var.get_value('a'));
+
+'''
+def __init__():
+    global glb_dict;
+    glb_dict = {};
+
+def set_value(key, value):
+    #set global var
+    glb_dict[key] = value;
+
+def get_value(key):
+    try:
+        return glb_dict[key];
+    except KeyError:
+        raise CustomException('The retrieved key does not exist');
