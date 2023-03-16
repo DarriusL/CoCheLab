7a0cb3620ece254f54b7fec53504355ac383b132
DarriusL <darrius.lei@outlook.com>
2023-03-16 21:08:37 +0800

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+from model.framework.cl4srec import *
+from model.framework.duo4srec import *
+from model.framework.ec4srec import *
+from model.framework.psac import *
+__all__ = ['CL4SRec', 'Duo4SRec', 'EC4SRec', "PSAC_gen"];
