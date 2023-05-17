92cb64f3f8828dc569b7164db0a1c1ef6d79f7b5
DarriusL <darrius.lei@outlook.com>
2023-05-17 09:54:01 +0800

    [2023.05.17]fixed some bugs.

--- executor.py
+++ executor.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     ).get_log()
     glb_var.set_value('logger', log);
     parse = argparse.ArgumentParser();
-    parse.add_argument('--data_process', '-dp', type = str, default = False, help = 'Whether to process data(True/False)');
+    parse.add_argument('--data_process', '-dp', type = bool, default = False, help = 'Whether to process data(True/False)');
     parse.add_argument('--config', '-cfg', type = str, default = None, help = 'config for run');
     parse.add_argument('--saved_config', '-sc', type = str, default = None, help = 'path for saved config to test')
     parse.add_argument('--mode', type = str, default = 'train', help = 'train/test/train_and_test')
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     #execute work command
     if args.config is not None:
         if args.mode not in ['train', 'train_and_test']:
-            log.error('The using of the configuration file does not support the [test] mode. \n'
+            log.error(f'The using of the configuration file does not support the {args.mode} mode. \n'
                       'Prompt: first use the [train] mode and then use the command [-- saved_config] to run the [test] mode, \n'
                       'or directly use the [train_and_test] mode');
             raise CustomException('ModeError');
